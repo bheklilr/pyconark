0750ba389727e4b9384e4094b8363fa24fc627b6
lemonprogis <1les-paul>
2018-09-12 16:56:59 -0500

    add message to models.py

--- main/models.py
+++ main/models.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# i have a google drive spreadsheet for us to look at 
+# how the models could be before we create them.
