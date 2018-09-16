7ce5c58f844e54de63727fdcb43318233e4e2751
adamclmns <adam@adamclmns.com>
2018-09-16 16:12:20 -0500

    tweaking the settings so that the install_main command will run without errors.

--- pyconark/settings.py
+++ pyconark/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
