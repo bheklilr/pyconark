7ce74ca21047bdefa5331f91bf573c551fa5a117
adamclmns <adam@adamclmns.com>
2018-09-22 15:47:21 -0500

    fixing a build issue that didn't show up until heroku deploy

--- main/admin.py
+++ main/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import ConferenceDetail, ConferenceLocation, ConferencePage, ConferenceSponsor, Speaker, SessionLocation, \
+from .models import ConferenceDetail, ConferenceLocation, ConferencePage, ConferenceSponsor, Speaker, SessionLocation, \
     Proposal, Price
 
 # Register your models here.
