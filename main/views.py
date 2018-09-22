06c8e437e146ca000b3b989410e1851e242d0f3e
adamclmns <adam@adamclmns.com>
2018-09-22 14:25:28 -0500

    comitting my changes so far

--- main/views.py
+++ main/views.py
@@ -7,4 +7,25 @@ from django.shortcuts import render
 # TODO: Implement some views
 def index(request):
     ''' home page '''
-    return render(request, 'main/index.html', {})
+    speakers = []
+    speakers.append({
+        "name": "BOB JOHNSON",
+        "contact": "1234567890",
+        "description": "email@domain.com"
+    })
+
+    schedule = []
+    schedule.append({
+        "title": "TITLE",
+        "name": "NAME",
+        "room": "ROOM",
+        "time": "TIME"
+    })
+    schedule.append({
+        "title": "TITLE",
+        "name": "NAME",
+        "room": "ROOM",
+        "time": "TIME"
+    })
+
+    return render(request, 'main/index.html', {'speakers': speakers, 'schedule': schedule})
