311e81eb7207a351f68d1c8327a5cb8e8c7ae428
adamclmns <adam@adamclmns.com>
2018-09-22 15:29:56 -0500

    Adding all of the models developed so far into the admin site so we can start building the data up to drive this thing.

--- main/admin.py
+++ main/admin.py
@@ -3,5 +3,16 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from models import ConferenceDetail, ConferenceLocation, ConferencePage, ConferenceSponsor, Speaker, SessionLocation, \
+    Proposal, Price
+
 # Register your models here.
 # TODO: Admin Models and/or Forms
+admin.site.register(ConferenceLocation)
+admin.site.register(ConferencePage)
+admin.site.register(ConferenceSponsor)
+admin.site.register(ConferenceDetail)
+admin.site.register(Speaker)
+admin.site.register(SessionLocation)
+admin.site.register(Proposal)
+admin.site.register(Price)
