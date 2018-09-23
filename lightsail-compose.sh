9aecd37978e12d319cece10db4aa778e78980e15
scottshellabarger <scott.shellabarger@gmail.com>
2018-09-23 15:38:04 -0500

    Updatew for testing

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -14,7 +14,7 @@ chmod +x /usr/local/bin/docker-compose
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
 mkdir /srv/docker
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/bheklilr/pyconark/master/docker-compose.yml
+curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/bheklilr/pyconark/tree/Scott/docker-compose.yml
 
 # copy in systemd unit file and register it so our compose file runs
 # on system restart
