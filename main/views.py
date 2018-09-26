c94ba3a618743022c6fd5f8390052d1557a64bec
lemonprogis <1les-paul>
2018-09-26 00:49:11 -0400

    add calls to models to view

--- main/views.py
+++ main/views.py
@@ -3,45 +3,19 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from .models import ConferenceDetail, NavBarItem
+from .models import *
 
 
 # TODO: Implement some views
 def index(request):
     ''' home page 
     '''
-    
-    speakers = []
-    speakers.append({
-        "name": "BOB JOHNSON",
-        "company": "Initech",
-        "phone": "1234567890",
-        "bio": "A short description about the speaker's background and qualifications",
-        "email": "email@domain.com"
-    })
-    speakers.append({
-        "name": "Wally West",
-        "company": "Star Labs",
-        "phone": "1234567890",
-        "bio": "This guy runs really fast. He's famous, and never screws up the timeline by time traveling.",
-        "email": "email@domain.com"
-    })
-    schedule = []
-    schedule.append({
-        "title": "TITLE",
-        "name": "NAME",
-        "room": "ROOM",
-        "time": "TIME"
-    })
-    schedule.append({
-        "title": "TITLE",
-        "name": "NAME",
-        "room": "ROOM",
-        "time": "TIME"
-    })
     navbar = NavBarItem.objects.filter(isActive=True)
-
+    speakers = speakers.objects.all()
+    schedule = Proposal.objects.filter(isActive=True)
     conferenceDetail = ConferenceDetail.objects.all()
+
+    
     # TODO: Fix this so it's not so dangerous....
     return render(request, 'main/index.html',
                   {'speakers': speakers,
